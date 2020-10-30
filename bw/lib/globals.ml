c1cdcecc36d3def85a03b29d657247125a9e437b
Nick Rioux <nrioux@seas.upenn.edu>
2020-10-30 10:53:46 -0400

    Proof terms, proof checker

--- bw/lib/globals.ml
+++ bw/lib/globals.ml
@@ -3,12 +3,22 @@ open Util
 module HTm = Hashcons.Make(Tm_rep)
 module PProp = Hashcons.Make(Prop_rep.PPropRep)
 module NProp = Hashcons.Make(Prop_rep.NPropRep)
-module HProof = Hashcons.Make (Proof_rep)
+module PPat = Hashcons.Make(Proof_rep.PPatRep)
+module NPat = Hashcons.Make(Proof_rep.NPatRep)
+module HProof = Hashcons.Make (Proof_rep.ProofRep)
+module PValue = Hashcons.Make (Proof_rep.ValueRep)
+module NStack = Hashcons.Make (Proof_rep.StackRep)
 
 module type T =
 sig
   type tm_t = HTm.t
   val tm_table : tm_t
+
+  type ppat_t = PPat.t
+  val ppat_table : ppat_t
+
+  type npat_t = NPat.t
+  val npat_table : npat_t
 
   type pprop_t = PProp.t
   val pprop_table : pprop_t
@@ -18,6 +28,12 @@ sig
 
   type proof_t = HProof.t
   val proof_table : proof_t
+
+  type value_t = PValue.t
+  val value_table : value_t
+
+  type stack_t = NStack.t
+  val stack_table : stack_t
 
   type sym_t = (int, (string * int)) Hashtbl.t
   val sym_table : sym_t
--- bw/lib/proof.ml
+++ bw/lib/proof.ml
@@ -1,286 +1,172 @@
 module type S = sig
   open Proof_rep
+  open Proof_rep.ProofRep
+  open Proof_rep.ValueRep
+  open Proof_rep.StackRep
   open Prop_rep
-  open Tm_rep
+  type ppat = int Proof_rep.ppat
+  type npat = int Proof_rep.npat
 
-  val pr_var : int -> proof
+  val pat_p_unit : ppat
+  val pat_p_inj : tag -> ppat -> ppat
+  val pat_p_sigpair : ppat -> ppat
+  val pat_p_var : int -> ppat
 
-  val pr_p_one : unit -> proof
+  val pat_n_proj : tag -> npat -> npat
+  val pat_n_app : ppat -> npat -> npat
+  val pat_n_piapp : npat -> npat
+  val pat_n_covar : npat
 
-  val pr_p_inL : proof -> pprop -> proof
+  val pr_p_match  : (ppat -> proof) -> proof (* pattern match on the next variable *)
+  val pr_n_match  : (npat -> proof) -> proof (* co-pattern matching i.e. lambda or matching on the current stack *)
+  val pr_p_rfoc   : value -> proof
+  val pr_n_lfoc   : int -> stack -> proof
 
-  val pr_p_inR : pprop -> proof -> proof
-
-  val pr_p_case : proof -> proof -> proof -> proof
-
-  val pr_p_sigpair : pprop -> tm -> proof -> proof
-
-  val pr_p_sigmatch : proof -> proof -> proof
-
-  val pr_p_shift : proof -> proof
-
-  val pr_n_top : unit -> proof
-
-  val pr_n_pair : proof -> proof -> proof
-
-  val pr_n_projL : proof -> proof -> proof
-
-  val pr_n_projR : proof -> proof -> proof
-
-  val pr_n_abs : pprop -> proof -> proof
-
-  val pr_n_app : proof -> proof -> proof -> proof
-
-  val pr_n_piabs : proof -> proof
-
-  val pr_n_piapp : proof -> tm -> proof -> proof
-
-  val pr_n_shift : proof -> proof
-
-  type context_item = Term | Prop of prop
-
-  type context = context_item list
-
-  val empty_context : context
-
-  (* these functions return `None` if the index is invalid in the provided context
-     else they return the type of that index, along with the offset for that item's
-     context *)
-  (* val nth_prop : context -> int -> (prop * context) option *)
-  (* val nth_term : context -> int -> context option *)
-
-  val infer_type : context -> proof -> prop option
+  val check       : (int * nprop) list -> pprop list -> proof -> nprop -> bool
+  val check_value : (int * nprop) list -> pprop list -> value -> pprop -> bool
+  val check_stack : (int * nprop) list -> pprop list -> stack -> nprop -> nprop -> bool
 end
 
-module Make (G : Globals.T) (TMS : Tm.S) (PRPS : Prop.S) = (* : S *) struct
-  open Proof_rep
+module Make (G : Globals.T) (TMS : Tm.S) (PRPS : Prop.S) : S = struct
+  open Proof_rep.ProofRep
+  open Proof_rep.ValueRep
+  open Proof_rep.StackRep
   open Prop_rep
-  open Tm_rep
   open List
+  module PPatRep = Proof_rep.PPatRep
+  module NPatRep = Proof_rep.NPatRep
+  type ppat = int Proof_rep.ppat
+  type npat = int Proof_rep.npat
 
-  let pr_var (i : int) = Globals.HProof.hashcons G.proof_table (Pr_var i)
+  let pat_p_unit : ppat = Globals.PPat.hashcons G.ppat_table Pat_p_unit
+  let pat_p_inj tag ppat : ppat = Globals.PPat.hashcons G.ppat_table (Pat_p_inj (tag, ppat))
+  let pat_p_sigpair ppat : ppat = Globals.PPat.hashcons G.ppat_table (Pat_p_sigpair ppat)
+  let pat_p_var v : ppat = Globals.PPat.hashcons G.ppat_table (Pat_p_var v)
 
-  let pr_p_one () = Globals.HProof.hashcons G.proof_table Pr_p_one
+  let pat_n_proj tag npat : npat = Globals.NPat.hashcons G.npat_table (Pat_n_proj (tag, npat))
+  let pat_n_app ppat npat : npat = Globals.NPat.hashcons G.npat_table (Pat_n_app (ppat, npat))
+  let pat_n_piapp npat : npat = Globals.NPat.hashcons G.npat_table (Pat_n_piapp npat)
+  let pat_n_covar : npat = Globals.NPat.hashcons G.npat_table Pat_n_covar
 
-  let pr_p_inL pr_l pp_r =
-    Globals.HProof.hashcons G.proof_table (Pr_p_inL (pr_l, pp_r))
+  let pr_p_match body : proof = Globals.HProof.hashcons G.proof_table (Pr_p_match body)
+  let pr_n_match body : proof = Globals.HProof.hashcons G.proof_table (Pr_n_match body)
+  let pr_p_rfoc value : proof = Globals.HProof.hashcons G.proof_table (Pr_p_rfoc value)
+  let pr_n_lfoc var stack : proof = Globals.HProof.hashcons G.proof_table (Pr_n_lfoc (var, stack))
 
-  let pr_p_inR pp_l pr_r =
-    Globals.HProof.hashcons G.proof_table (Pr_p_inR (pp_l, pr_r))
+  let pr_value subst tms ppat = Globals.PValue.hashcons G.value_table (Pr_value (subst, tms, ppat))
+  let pr_stack subst tms k npat = Globals.NStack.hashcons G.stack_table (Pr_stack (subst, tms, k, npat))
 
-  let pr_p_case pr_disj pr_then pr_else =
-    Globals.HProof.hashcons G.proof_table
-      (Pr_p_case (pr_disj, pr_then, pr_else))
-
-  let pr_p_sigpair pp tm_witness pr =
-    Globals.HProof.hashcons G.proof_table (Pr_p_sigpair (pp, tm_witness, pr))
-
-  let pr_p_sigmatch pr_existential pr_cont =
-    Globals.HProof.hashcons G.proof_table
-      (Pr_p_sigmatch (pr_existential, pr_cont))
-
-  let pr_p_shift pr = Globals.HProof.hashcons G.proof_table (Pr_p_shift pr)
-
-  let pr_n_top () = Globals.HProof.hashcons G.proof_table Pr_n_top
-
-  let pr_n_pair pr_l pr_r =
-    Globals.HProof.hashcons G.proof_table (Pr_n_pair (pr_l, pr_r))
-
-  let pr_n_projL pr_conj pr_cont =
-    Globals.HProof.hashcons G.proof_table (Pr_n_projL (pr_conj, pr_cont))
-
-  let pr_n_projR pr_conj pr_cont =
-    Globals.HProof.hashcons G.proof_table (Pr_n_projR (pr_conj, pr_cont))
-
-  let pr_n_abs pp_arg pr_body =
-    Globals.HProof.hashcons G.proof_table (Pr_n_abs (pp_arg, pr_body))
-
-  let pr_n_app pr_abs pr_arg pr_cont =
-    Globals.HProof.hashcons G.proof_table (Pr_n_app (pr_abs, pr_arg, pr_cont))
-
-  let pr_n_piabs pr_body =
-    Globals.HProof.hashcons G.proof_table (Pr_n_piabs pr_body)
-
-  let pr_n_piapp pr_piabs tm_arg pr_cont =
-    Globals.HProof.hashcons G.proof_table
-      (Pr_n_piapp (pr_piabs, tm_arg, pr_cont))
-
-  let pr_n_shift pr = Globals.HProof.hashcons G.proof_table (Pr_n_shift pr)
-
-  type context_item = Term | Prop of prop
-
-  type context = context_item list
-
-  let empty_context = []
-
-  let rec pprop_replace_terms (f : int -> tm -> tm) (p : pprop) (d : int) :
-      pprop =
-    match p.node with
-    | P_one -> PRPS.p_one ()
-    | P_zero -> PRPS.p_zero ()
-    | P_or (p1, p2) ->
-        PRPS.p_or (pprop_replace_terms f p1 d) (pprop_replace_terms f p2 d)
-    | P_ex p -> PRPS.p_ex (pprop_replace_terms f p (d + 1))
-    | P_shift p -> PRPS.p_shift (nprop_replace_terms f p d)
-
-  and nprop_replace_terms (f : int -> tm -> tm) (p : nprop) (d : int) : nprop =
-    match p.node with
-    | N_prop (tag, tms) ->
-        let tms' = map (fun tm -> f d tm) tms in
-        PRPS.n_prop tag tms'
-    | N_top -> PRPS.n_top ()
-    | N_and (p1, p2) ->
-        PRPS.n_and (nprop_replace_terms f p1 d) (nprop_replace_terms f p2 d)
-    | N_imp (p1, p2) ->
-        PRPS.n_imp (pprop_replace_terms f p1 d) (nprop_replace_terms f p2 d)
-    | N_all p -> PRPS.n_all (nprop_replace_terms f p (d + 1))
-    | N_shift p -> PRPS.n_shift (pprop_replace_terms f p d)
-
-  (* this replaces free variable `i` with `t` and decrememnts other variable indices as appropriate *)
-  let prop_replace_terms (f : int -> tm -> tm) (p : prop) (d : int) : prop =
-    match p with
-    | Pos p' -> Pos (pprop_replace_terms f p' d)
-    | Neg p' -> Neg (nprop_replace_terms f p' d)
-
-  let rec term_replace_uvars (f : int -> tm) (t : tm) : tm =
-    match t.node with
-    | Tm_uvar i -> f i
-    | Tm_param tag -> TMS.tm_param tag
-    | Tm_fun (tag, tms) -> TMS.tm_fun tag (map (term_replace_uvars f) tms)
-
-  (* this is sortof the reverse of `subst_for_free_var`, in that it will increment variable indices
-   * except that this will do it multiple (n) times. in particular, dooing `n` substitutions on the same
-   * index `i`, followed by this function, should result in the original term, with substitutions, but
-   * *without* changing the indices of any other variables *)
-  let add_free_term_vars (first_idx_to_incr : int) (incr_count : int) : tm -> tm
-      =
-    term_replace_uvars (fun (leaf_idx : int) ->
-        if leaf_idx < first_idx_to_incr then TMS.tm_uvar leaf_idx
-        else TMS.tm_uvar (leaf_idx + incr_count))
-
-  let subst_for_free_term_var (p : prop) (t : tm) (idx_to_replace : int) : prop
-      =
-    let f (depth : int) (leaf_idx : int) : tm =
-      let idx_to_replace_adjusted = idx_to_replace + depth in
-      if leaf_idx = idx_to_replace_adjusted then
-        (* do the substitution: we mangle the variables in t to account for how many binders deep we are, then we return it *)
-        add_free_term_vars 0 depth t
-      else if leaf_idx < idx_to_replace_adjusted then
-        (* this variable refers to an index lower than the one we're replacing, so it is unaffected *)
-        TMS.tm_uvar leaf_idx
-      else
-        (* this variable refers to an index higher than the one we're replacing, so we have to shift it down one *)
-        TMS.tm_uvar (leaf_idx - 1)
-    in
-
-    prop_replace_terms (fun (depth : int) -> term_replace_uvars (f depth)) p 0
-
-  (* gets the nth prop from the context, if it exists, and ajusts its de Brujin indicies to be valid in the current context *)
-  type _get_mangled_var_state = {
-    term_count : int;
-    prop_count : int;
-    found : prop option;
+  type ppat_rec = {
+    pat : ppat;
+    vars : (int * nprop) list
   }
+  type ppat_set = ppat_rec list
 
-  let get_mangled_var (gamma : context) (n : int) : prop option =
-    let f (state : _get_mangled_var_state) (item : context_item) =
-      match (state.found = None, item) with
-      | false, _ -> state
-      | true, Term -> { state with term_count = state.term_count + 1 }
-      | true, Prop p ->
-          {
-            state with
-            prop_count = state.prop_count + 1;
-            found = (if state.prop_count = n then Some p else None);
-          }
-    in
-    match
-      fold_left f { term_count = 0; prop_count = 0; found = None } gamma
-    with
-    (* when we return the prop from the context, we need to adjust the term variable indices in it to account for the
-     * additional items in the context. however we only adjust those variables that are free, which is why we only
-     * change those with index >= depth *)
-    | { found = Some p; term_count; _ } ->
-        Some
-          (prop_replace_terms
-             (fun depth -> add_free_term_vars depth term_count)
-             p 0)
-    | _ -> None
+  type npat_rec = {
+    pat : npat;
+    (* types of variables in the pattern *)
+    vars : (int * nprop) list;
+    (* type of the output *)
+    out : nprop;
+  }
+  type npat_set = npat_rec list
 
-  (* these should roughly match the rules from McLaughlin and Pfenning's "Efficient Intuitionistic Theorem Proving with the Polarized Inverse Method" *)
-  let rec infer_type gamma (proof : proof) =
-    match proof.node with
-    | Pr_var i -> get_mangled_var gamma i
-    | Pr_p_one -> Some (Pos (PRPS.p_one ()))
-    | Pr_p_inL (t1, p2) -> (
-        match infer_type gamma t1 with
-        | Some (Pos p1') -> Some (Pos (PRPS.p_or p1' p2))
-        | _ -> None )
-    | Pr_p_inR (p1, t2) -> (
-        match infer_type gamma t2 with
-        | Some (Pos p2') -> Some (Pos (PRPS.p_or p1 p2'))
-        | _ -> None )
-    | Pr_p_case (c, t1, t2) -> (
-        match infer_type gamma c with
-        | Some (Pos { node = P_or (q1, q2); _ }) ->
-            let p1 = infer_type (Prop (Pos q1) :: gamma) t1 in
-            let p2 = infer_type (Prop (Pos q2) :: gamma) t2 in
-            if p1 = p2 then p1 else None
-        | _ -> None )
-    | Pr_p_shift t -> (
-        match infer_type gamma t with
-        | Some (Neg p) -> Some (Pos (PRPS.p_shift p))
-        | _ -> None )
-    | Pr_n_top -> Some (Neg (PRPS.n_top ()))
-    | Pr_n_pair (t1, t2) -> (
-        match (infer_type gamma t1, infer_type gamma t2) with
-        | Some (Neg p1), Some (Neg p2) -> Some (Neg (PRPS.n_and p1 p2))
-        | _ -> None )
-    | Pr_n_projL (p, c) -> (
-        match infer_type gamma p with
-        | Some (Neg { node = N_and (p1, _); _ }) ->
-            infer_type (Prop (Neg p1) :: gamma) c
-        | _ -> None )
-    | Pr_n_projR (p, c) -> (
-        match infer_type gamma p with
-        | Some (Neg { node = N_and (_, p2); _ }) ->
-            infer_type (Prop (Neg p2) :: gamma) c
-        | _ -> None )
-    | Pr_n_abs (q, t) -> infer_type (Prop (Pos q) :: gamma) t
-    (*
-      G |- t1 : P     G, q : Q |- t2 : T
-      -------------------------------------
-      G', f : P -> Q |- T_N_app (var f) t1 t2 : T
-     *)
-    | Pr_n_app (f, x, c) -> (
-        let pf = infer_type gamma f in
-        let px = infer_type gamma x in
-        match (pf, px) with
-        | Some (Neg { node = N_imp (pf1, pf2); _ }), Some (Pos px') ->
-            if px' = pf1 then infer_type (Prop (Neg pf2) :: gamma) c else None
-        | _ -> None )
-    | Pr_n_shift t -> (
-        match infer_type gamma t with
-        | Some (Pos p) -> Some (Neg (PRPS.n_shift p))
-        | _ -> None )
-    | Pr_p_sigpair (p, x, t) ->
-        if Some (subst_for_free_term_var (Pos p) x 0) = infer_type gamma t then
-          Some (Pos (PRPS.p_ex p))
-        else None
-    | Pr_p_sigmatch (ex, t) -> (
-        match infer_type gamma ex with
-        (* we don't need to mangle the indices in `p`, since while part of `P_ex p`, term index 0
-         * referred to the thing that was existentially quantified, and now because of the positions
-         * in the context, 0 will refer to the new term variable *)
-        | Some (Pos { node = P_ex p; _ }) ->
-            infer_type (Prop (Pos p) :: Term :: gamma) t
-        | _ -> None )
-    | Pr_n_piabs t -> infer_type (Term :: gamma) t
-    | Pr_n_piapp (f, x, c) -> (
-        let pf = infer_type gamma f in
-        match pf with
-        | Some (Neg { node = N_all pf'; _ }) ->
-            let pf'' = subst_for_free_term_var (Neg pf') x 0 in
-            infer_type (Prop pf'' :: gamma) c
-        | _ -> None )
+  (** Generate all the fully-expanded patterns that can match against the given proposition. *)
+  let rec pat_for_p (prop : pprop) (fresh : unit -> 'a) : ppat_set =
+    match prop.node with
+    | P_one -> [{pat = pat_p_unit; vars = []}]
+    | P_zero -> []
+    | P_or (prop1, prop2) ->
+      let inl = List.map (fun ({pat; vars} : ppat_rec) -> {pat = pat_p_inj Proof_rep.Left pat; vars}) (pat_for_p prop1 fresh) in
+      let inr = List.map (fun ({pat; vars} : ppat_rec) -> {pat = pat_p_inj Proof_rep.Right pat; vars}) (pat_for_p prop2 fresh) in
+      inl @ inr
+    | P_ex prop' ->
+      List.map (fun ({pat; vars} : ppat_rec) -> {pat = pat_p_sigpair pat; vars}) (pat_for_p prop' fresh)
+    | P_shift nprop ->
+      let v = fresh () in
+      [{pat = pat_p_var v; vars = [(v, nprop)]}]
+
+  (** Generate all the fully-expanded co-patterns that can match against the given proposition. *)
+  let rec pat_for_n (prop: nprop) (fresh : unit -> 'a) : npat_set =
+    match prop.node with
+    | N_prop (_, _) -> [{ pat = pat_n_covar; vars = []; out = prop }]
+    | N_top -> []
+    | N_and (prop1, prop2) ->
+      let projl = List.map (fun {pat; vars; out} ->
+                              { pat = pat_n_proj Proof_rep.Left pat;
+                                vars; out })
+                           (pat_for_n prop1 fresh)
+      in
+      let projr = List.map (fun {pat; vars; out} ->
+                              { pat = pat_n_proj Proof_rep.Right pat;
+                                vars; out })
+                           (pat_for_n prop2 fresh) in
+      projl @ projr
+    | N_imp (pprop, nprop) ->
+      let ppats = pat_for_p pprop fresh in
+      let npats = pat_for_n nprop fresh in
+      List.concat_map (fun ({pat = ppat; vars = vars1} : ppat_rec) ->
+        List.map (fun {pat = npat; vars = vars2; out} -> { pat = pat_n_app ppat npat; vars = vars1 @ vars2; out })
+                 npats)
+        ppats
+    | N_all prop' ->
+      List.map (fun {pat; vars; out} -> {pat = pat_n_piapp pat; vars; out}) (pat_for_n prop' fresh)
+    | N_shift _ ->
+      [{pat = pat_n_covar; vars = []; out = prop}]
+
+  let rec check (stable_env : (int * nprop) list) (env : pprop list) (proof : proof)  (nprop : nprop) : bool =
+    (* TODO how to we avoid clashing wth free variables already in proof???? *)
+    let counter = ref 0 in
+    let fresh = (fun () -> let name = !counter in counter := name + 1; name) in
+    match proof.node, env, nprop.node with
+    | Pr_p_match body, pprop :: env', _ ->
+      List.for_all (fun ({pat; vars} : ppat_rec) -> check (vars @ stable_env) env' (body pat) nprop) (pat_for_p pprop fresh)
+    | Pr_n_match body, _, _ ->
+      List.for_all (fun ({pat; vars; out} : npat_rec) -> check (vars @ stable_env) env (body pat) out) (pat_for_n nprop fresh)
+    | Pr_p_rfoc value, _, N_shift pprop ->
+      check_value stable_env env value  pprop
+    | Pr_n_lfoc (v, stack), _, _ ->
+      check_stack stable_env env stack (List.assoc v stable_env) nprop
+    | _, _, _ -> false
+
+and check_value (stable_env : (int * nprop) list) (env : pprop list) (value : value)  (pprop : pprop) : bool =
+  match value.node, pprop.node with
+  | Pr_value (_, _, {node = Pat_p_unit; _}), P_one -> true
+  | Pr_value (subst, tms, {node = Pat_p_inj (tag, ppat); _}), P_or (pprop1, pprop2) ->
+    let value' = pr_value subst tms ppat in
+    begin match tag with
+      | Left -> check_value stable_env env value' pprop1
+      | Right -> check_value stable_env env value' pprop2
+    end
+  | Pr_value (subst, tm::tms, {node = Pat_p_sigpair ppat; _}), P_ex pprop_body ->
+    let value' = pr_value subst tms ppat in
+    let pprop' = PRPS.open_pt tm pprop_body in
+    check_value stable_env env value' pprop'
+  | Pr_value (subst, _, {node = Pat_p_var v; _}), P_shift nprop ->
+    check stable_env env (subst v) nprop ||
+    NPropRep.equal (List.assoc v stable_env).node nprop.node
+  | _, _ -> false
+
+and check_stack (stable_env : (int * nprop) list) (env : pprop list) (stack : stack) (nprop_hole : nprop) (nprop_ret : nprop) : bool =
+  match stack.node, nprop_hole.node, nprop_ret.node with
+  | Pr_stack (_, [], None, {node = Pat_n_covar; _}), N_prop (_, _), N_prop (_, _) ->
+    NPropRep.equal nprop_hole.node nprop_ret.node
+  | Pr_stack (subst_p, tms, subst_n, {node = Pat_n_proj (tag, npat); _}), N_and (nprop_l, nprop_r), _ ->
+    let stack' = pr_stack subst_p tms subst_n npat in
+    begin match tag with
+      | Left ->  check_stack stable_env env stack' nprop_l nprop_ret
+      | Right -> check_stack stable_env env stack' nprop_r nprop_ret
+    end
+  | Pr_stack (subst_p, tms, subst_n, {node = Pat_n_app (ppat, npat); _}), N_imp (pprop, nprop), _ ->
+    let stack' = pr_stack subst_p tms subst_n npat in
+    let value = pr_value subst_p tms ppat in
+    check_value stable_env env value pprop &&
+    check_stack stable_env env stack' nprop nprop_ret
+  | Pr_stack (subst_p, tm::tms, subst_n, {node = Pat_n_piapp npat; _}), N_all nprop, _ ->
+    let stack' = pr_stack subst_p tms subst_n npat in
+    let nprop_hole' = PRPS.open_nt tm nprop in
+    check_stack stable_env env stack' nprop_hole' nprop_ret
+  | Pr_stack (_, [], Some k, {node = Pat_n_covar; _}), N_shift pprop, _ ->
+    check stable_env (pprop :: env) k nprop_ret
+  | _, _, _ -> false
 end
